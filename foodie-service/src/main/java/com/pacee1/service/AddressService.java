a52b65b286c9db5cca112032d053452d4f49f1fa
Pace <8709867@qq.com>
2020-06-11 17:39:36 +0800

    add 用户地址功能

---
+++ foodie-service/src/main/java/com/pacee1/service/AddressService.java
@@ -0,0 +1,33 @@
+package com.pacee1.service;
+
+import com.pacee1.pojo.UserAddress;
+import com.pacee1.pojo.Users;
+import com.pacee1.pojo.bo.UserAddressBO;
+import com.pacee1.pojo.bo.UserBO;
+
+import java.util.List;
+
+/**
+ * @author pace
+ * @version v1.0
+ * @Type UserService.java
+ * @Desc
+ * @date 2020/5/17 15:16
+ */
+public interface AddressService {
+
+    /**
+     * 查
+     * @param userId
+     * @return
+     */
+    List<UserAddress> queryUserAddress(String userId);
+
+    void add(UserAddressBO userAddressBO);
+
+    void edit(UserAddressBO userAddressBO);
+
+    void delete(String userId,String addressId);
+
+    void setDefaultAddress(String userId,String addressId);
+}
